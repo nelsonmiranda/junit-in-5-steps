9f46806bc92b63461c183b7435e98789025ef9a5
Nelson Miranda <nelsonmiranda8421@gmail.com>
2020-06-02 20:29:20 -0600

    JUnit project and green bar

---
+++ test/com/in28minutes/junit/MyMathTest.java
@@ -0,0 +1,14 @@
+package com.in28minutes.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyMathTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+	}
+
+}
