6bb92919766e833687d674c327f0dd347cb07ea0
Nelson Miranda <nelsonmiranda8421@gmail.com>
2020-06-02 21:13:40 -0600

    Execution order of each annotation

--- test/com/in28minutes/junit/MyMathTest.java
+++ test/com/in28minutes/junit/MyMathTest.java
@@ -1,15 +1,40 @@
 package com.in28minutes.junit;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MyMathTest {
 
+	@AfterEach
+	public void after() {
+		System.out.println("After Each");
+	}
+
+	@AfterAll
+	public static void afterClass() {
+		System.out.println("After All");
+	}
+
+	@BeforeAll
+	public static void beforeClass() {
+		System.out.println("Before All");
+	}
+
+	@BeforeEach
+	public void before() {
+		System.out.println("Before Each");
+	}
+
 	@Test
 	void sum() {
+		System.out.println("Test");
 		MyMath myMath = new MyMath();
-		assertEquals(50, myMath.sum(new int[] {10,25,15}));
+		assertEquals(50, myMath.sum(new int[] { 10, 25, 15 }));
 	}
 
 }
